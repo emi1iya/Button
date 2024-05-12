ebbda99dd62dea226a20af4ffb4dba46a0144f66
emilia <ahmerovaemilia@yandex.ru>
2024-05-12 10:30:54 -0700

    Button

--- Button/ViewController.swift
+++ Button/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var emptyLabel: UILabel!
+    var counter:Int = 0
+    @IBAction func button(_ sender: Any) {
+        counter+=1
+        emptyLabel.text = "\(counter)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
